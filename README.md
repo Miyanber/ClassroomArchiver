15631f0a92212e24672ee5a0c385283725018091
Miyanber <miyanba2984h@gmail.com>
2026-03-06 11:47:19 +0900

    コード整理

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # ClassroomArchiver
 Export your Google Classroom into a fully browsable HTML archive.
+
+
+## 説明
+
+- 所属するクラスのお知らせ・課題・資料が、HTML形式でローカルに保存されます。
+
+## 注意事項
+
+ 以下の内容はローカルに保存されません。ご注意ください。
+<ul>
+    <li>投稿に対するコメント</li>
+    <li>課題内の限定公開のコメント</li>
+    <li>ドライブファイル内のコメント</li>
+    <li>Google Forms</li>
+</ul>
+　　
+
+以下の内容はローカルには保存されませんが、マイドライブの ClassroomArchive フォルダ内にファイルのコピーが作成されます。
+ HTML ファイル内でのリンク先は、コピーされたファイルのリンクに変わります。
+※マイドライブはアカウント停止と同時にアクセスできなくなります。アカウント移行時に Google Drive を移行内容に含めることで、データを保持することができます。
+
+<ul>
+    <li>Google Docs ファイル</li>
+    <li>Google Slides ファイル</li>
+    <li>Google Sheets ファイル</li>
+</ul>
--- main.py
+++ main.py
@@ -11,6 +11,12 @@ from datetime import datetime, timezone, timedelta
 import shutil
 from tqdm import tqdm
 
+import signal
+import os
+signal.signal(signal.SIGINT, lambda sig, frame: stop_event.set())
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 jst_today = datetime.now().astimezone(timezone(timedelta(hours=9)))
 jst_today_str = jst_today.strftime("%Y%m%d%H%M%S")
@@ -51,25 +57,43 @@ try:
     file_name = "archive_folder_id.txt"
     drive_service = build("drive", "v3", credentials=creds)
 
-    if os.path.exists(file_name):
-        with open(file_name, "r") as f:
-            archive_folder_id = f.read()
-    else:
+    folder_name = "Classroom Archive"
+
+    query = (
+        f"name = '{folder_name}' "
+        f"and 'root' in parents "
+        f"and mimeType = 'application/vnd.google-apps.folder' "
+        f"and trashed = false"
+    )
+
+    results = drive_service.files().list(
+        q=query,
+        spaces='drive',
+        fields='files(id, name)', # 必要なフィールドだけ取得
+        pageSize=10
+    ).execute()
+    
+    items = results.get('files', [])
+    root_folder_id = None
+
+    if not items:
+        print(f"フォルダ '{folder_name}' は見つかりませんでした。")
         file_metadata = {
             "name": "Classroom Archive",
             "mimeType": "application/vnd.google-apps.folder",
         }
-
         file = drive_service.files().create(body=file_metadata, fields="id").execute()
-        archive_folder_id = file.get("id")
-        with open(file_name, "w") as f:
-            f.write(archive_folder_id)
+        root_folder_id = file.get("id")
+    else:
+        # 複数ヒットする可能性があるため、最初の一つを返す
+        folder = items[0]
+        root_folder_id = folder["id"]
 
     # 個別フォルダ作成
     file_metadata = {
         "name": jst_today_str,
         "mimeType": "application/vnd.google-apps.folder",
-        "parents": [archive_folder_id],
+        "parents": [root_folder_id],
     }
     file = drive_service.files().create(body=file_metadata, fields="id").execute()
     archive_folder_id = file.get("id")
@@ -110,52 +134,6 @@ stop_event = threading.Event()
 user_profiles = {}
 pictures_to_download = set()
 
-# for course in courses.get("courses", []):
-course = courses[4]
-print(f"クラス名: {course["name"]}")
-
-announcements = list_all(
-    lambda **kwargs: service.courses().announcements().list(courseId=course["id"], **kwargs),
-    "announcements"
-)
-course_works = list_all(
-    lambda **kwargs: service.courses().courseWork().list(courseId=course["id"], **kwargs),
-    "courseWork"
-)
-course_work_materials = list_all(
-    lambda **kwargs: service.courses().courseWorkMaterials().list(courseId=course["id"], **kwargs),
-    "courseWorkMaterial"
-)
-teachers = list_all(
-    lambda **kwargs: service.courses().teachers().list(courseId=course["id"], **kwargs),
-    "teachers"
-)
-topics = list_all(
-    lambda **kwargs: service.courses().topics().list(courseId=course["id"], **kwargs),
-    "topic"
-)
-submissions = list_all(
-    lambda **kwargs: service.courses().courseWork().studentSubmissions().list(
-        courseId=course["id"],
-        courseWorkId="-",
-        userId="me",
-        **kwargs
-    ),
-    "studentSubmissions"
-)
-
-for teacher in teachers:
-    if teacher["userId"] in user_profiles:
-        continue
-    profile = teacher["profile"]
-    user_profiles[teacher["userId"]] = profile
-    if "photoUrl" in profile:
-        path = f"{base_dir}/img/icons/{profile["id"]}.png"
-        if os.path.exists(path):
-            print(f"Skip (already exists): {path}.png;")
-        else:
-            pictures_to_download.add((f"https:{profile["photoUrl"]}", path))
-
 
 def get_jst_str(iso_str):
     utc_dt = datetime.fromisoformat(iso_str.replace('Z', '+00:00'))
@@ -163,18 +141,6 @@ def get_jst_str(iso_str):
     jst_dt = utc_dt.astimezone(jst_timezone)
     jst_dt_str = f"{jst_dt.year}年{jst_dt.month}月{jst_dt.day}日 {jst_dt.hour}時{jst_dt.minute}分"
     return jst_dt_str
-
-# 授業のトピック
-topic_map = {
-    topic["topicId"]: topic
-    for topic in topics
-}
-
-# 提出物（課題の添付ファイル）
-submission_map = {
-    s["courseWorkId"]: s
-    for s in submissions
-}
 
 def get_file_type_name(mime_type):
     if mime_type == None:
@@ -187,7 +153,7 @@ def get_file_type_name(mime_type):
         return "Google スプレッドシート"
     elif mime_type == "application/pdf":
         return "PDF"
-    
+
 
 # {mime_type': (None | str), 'was_saved': (bool)} を返す。
 def download_drive_file(file_id, filename):
@@ -256,64 +222,6 @@ def download_drive_file(file_id, filename):
     }
 
 
-def get_course_work_attachments(course_work):
-    submission = submission_map.get(course_work["id"])
-    if not submission:
-        return
-
-    assignmentSubmission = submission.get("assignmentSubmission")
-    if not assignmentSubmission:
-        return
-
-    attachments = assignmentSubmission.get("attachments", [])
-
-    course_work["attachments"] = attachments
-    for attachment in attachments:
-        if "driveFile" in attachment:
-            # Materialとのズレを修正するため
-            # テンプレートではMaterialと同様に扱う
-            attachment["driveFile"]["driveFile"] = attachment["driveFile"]
-            drive_file = attachment["driveFile"]["driveFile"]
-            if "title" in attachment["driveFile"]:
-                file_dict = download_drive_file(drive_file["id"], drive_file["title"])
-                drive_file["file_type_name"] = get_file_type_name(file_dict["mime_type"])
-                drive_file["was_saved"] = file_dict["was_saved"]
-                if "web_view_link" in file_dict:
-                    drive_file["web_view_link"] = file_dict["web_view_link"]
-
-
-def get_all_materials(item):
-    item["creatorUserProfile"] = user_profiles[item["creatorUserId"]]
-    item["creationTime"] = get_jst_str(item["creationTime"])
-    item["updateTime"] = get_jst_str(item["updateTime"])
-    item["was_updated"] = item["creationTime"] != item["updateTime"]
-    if "topicId" in item:
-        item["topicName"] = topic_map[item["topicId"]]["name"]
-
-    if "materials" in item:
-        for material in item["materials"]:
-            if "driveFile" in material and "title" in material["driveFile"]["driveFile"]:
-                drive_file = material["driveFile"]["driveFile"]
-                file_dict = download_drive_file(drive_file["id"], drive_file["title"])
-                drive_file["file_type_name"] = get_file_type_name(file_dict["mime_type"])
-                drive_file["was_saved"] = file_dict["was_saved"]
-                if "web_view_link" in file_dict:
-                    drive_file["web_view_link"] = file_dict["web_view_link"]
-
-
-for item in announcements:
-    item["item_type"] = "Announcement"
-for item in course_works:
-    item["item_type"] = "CourseWork"
-for item in course_work_materials:
-    item["item_type"] = "CourseWorkMaterial"
-
-
-all_items = announcements + course_works + course_work_materials
-# get_jst_str(item["creationTime"]) で変換する前に実行する必要あり
-all_items.sort(key=lambda item: item['updateTime'], reverse=True)
-
-
 def download_file(url, path):
     r = requests.get(url, timeout=10)
     if r.status_code == 200:
@@ -323,37 +231,147 @@ def download_file(url, path):
     else:
         print(f"Failed to save {path}.png; status_code: {r.status_code};")
 
-import signal
-import os
-signal.signal(signal.SIGINT, lambda sig, frame: stop_event.set())
 
-from concurrent.futures import ThreadPoolExecutor, as_completed
+# コース別の処理
+for course in courses:
+    print(course)
+    print(f"クラス名: {course["name"]}")
 
-try:
-    with ThreadPoolExecutor(max_workers=8) as executor:
-        # 先に全タスクをsubmitし、futureオブジェクトをリスト化する
-        futures = [executor.submit(download_file, picture[0], picture[1]) for picture in pictures_to_download]
-        futures += [executor.submit(get_course_work_attachments, item) for item in course_works]
-        futures += [executor.submit(get_all_materials, item) for item in all_items]
+    announcements = list_all(
+        lambda **kwargs: service.courses().announcements().list(courseId=course["id"], **kwargs),
+        "announcements"
+    )
+    course_works = list_all(
+        lambda **kwargs: service.courses().courseWork().list(courseId=course["id"], **kwargs),
+        "courseWork"
+    )
+    course_work_materials = list_all(
+        lambda **kwargs: service.courses().courseWorkMaterials().list(courseId=course["id"], **kwargs),
+        "courseWorkMaterial"
+    )
+    teachers = list_all(
+        lambda **kwargs: service.courses().teachers().list(courseId=course["id"], **kwargs),
+        "teachers"
+    )
+    topics = list_all(
+        lambda **kwargs: service.courses().topics().list(courseId=course["id"], **kwargs),
+        "topic"
+    )
+    submissions = list_all(
+        lambda **kwargs: service.courses().courseWork().studentSubmissions().list(
+            courseId=course["id"],
+            courseWorkId="-",
+            userId="me",
+            **kwargs
+        ),
+        "studentSubmissions"
+    )
+
+    for teacher in teachers:
+        if teacher["userId"] in user_profiles:
+            continue
+        profile = teacher["profile"]
+        user_profiles[teacher["userId"]] = profile
+        if "photoUrl" in profile:
+            path = f"{base_dir}/img/icons/{profile["id"]}.png"
+            if os.path.exists(path):
+                print(f"Skip (already exists): {path}.png;")
+            else:
+                pictures_to_download.add((f"https:{profile["photoUrl"]}", path))
+
+    # 授業のトピック
+    topic_map = {
+        topic["topicId"]: topic
+        for topic in topics
+    }
         
-        # as_completedで終わったものから取り出し、tqdmでラップする
-        results = []
-        for future in tqdm(as_completed(futures), total=len(futures), desc="ファイルを保存中"):
-            results.append(future.result())
+    # 提出物（課題の添付ファイル）
+    submission_map = {
+        s["courseWorkId"]: s
+        for s in submissions
+    }
 
-except KeyboardInterrupt:
-    stop_event.set()
+    def get_course_work_attachments(course_work):
+        submission = submission_map.get(course_work["id"])
+        if not submission:
+            return
 
-html = template.render(
-    name=course["name"],
-    section=course.get("section", ""),
-    announcements=announcements,
-    course_work=course_works,
-    course_work_materials=course_work_materials,
-    all_items=all_items
-)
+        assignmentSubmission = submission.get("assignmentSubmission")
+        if not assignmentSubmission:
+            return
 
-with open(f"{base_dir}/クラス_{course["name"]}.html", "w", encoding="utf-8") as f:
-    f.write(html)
+        attachments = assignmentSubmission.get("attachments", [])
+
+        course_work["attachments"] = attachments
+        for attachment in attachments:
+            if "driveFile" in attachment:
+                # Materialとのズレを修正するため
+                # テンプレートではMaterialと同様に扱う
+                attachment["driveFile"]["driveFile"] = attachment["driveFile"]
+                drive_file = attachment["driveFile"]["driveFile"]
+                if "title" in attachment["driveFile"]:
+                    file_dict = download_drive_file(drive_file["id"], drive_file["title"])
+                    drive_file["file_type_name"] = get_file_type_name(file_dict["mime_type"])
+                    drive_file["was_saved"] = file_dict["was_saved"]
+                    if "web_view_link" in file_dict:
+                        drive_file["web_view_link"] = file_dict["web_view_link"]
+
+
+    def get_all_materials(item):
+        item["creatorUserProfile"] = user_profiles[item["creatorUserId"]]
+        item["creationTime"] = get_jst_str(item["creationTime"])
+        item["updateTime"] = get_jst_str(item["updateTime"])
+        item["was_updated"] = item["creationTime"] != item["updateTime"]
+        if "topicId" in item:
+            item["topicName"] = topic_map[item["topicId"]]["name"]
+
+        if "materials" in item:
+            for material in item["materials"]:
+                if "driveFile" in material and "title" in material["driveFile"]["driveFile"]:
+                    drive_file = material["driveFile"]["driveFile"]
+                    file_dict = download_drive_file(drive_file["id"], drive_file["title"])
+                    drive_file["file_type_name"] = get_file_type_name(file_dict["mime_type"])
+                    drive_file["was_saved"] = file_dict["was_saved"]
+                    if "web_view_link" in file_dict:
+                        drive_file["web_view_link"] = file_dict["web_view_link"]
+
+
+    for item in announcements:
+        item["item_type"] = "Announcement"
+    for item in course_works:
+        item["item_type"] = "CourseWork"
+    for item in course_work_materials:
+        item["item_type"] = "CourseWorkMaterial"
+
+
+    all_items = announcements + course_works + course_work_materials
+    # get_jst_str(item["creationTime"]) で変換する前に実行する必要あり
+    all_items.sort(key=lambda item: item['updateTime'], reverse=True)
+
+    try:
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            # 先に全タスクをsubmitし、futureオブジェクトをリスト化する
+            futures = [executor.submit(download_file, picture[0], picture[1]) for picture in pictures_to_download]
+            futures += [executor.submit(get_course_work_attachments, item) for item in course_works]
+            futures += [executor.submit(get_all_materials, item) for item in all_items]
+            
+            # as_completedで終わったものから取り出し、tqdmでラップする
+            results = []
+            for future in tqdm(as_completed(futures), total=len(futures), desc="ファイルを保存中"):
+                results.append(future.result())
+
+    except KeyboardInterrupt:
+        stop_event.set()
+
+    html = template.render(
+        course=course,
+        announcements=announcements,
+        course_work=course_works,
+        course_work_materials=course_work_materials,
+        all_items=all_items
+    )
+
+    with open(f"{base_dir}/クラス_{course["name"]}.html", "w", encoding="utf-8") as f:
+        f.write(html)
 
 print(f"完了しました。アーカイブは {base_dir} に出力されています。")
