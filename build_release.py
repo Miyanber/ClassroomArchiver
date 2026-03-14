c19681291e162db0cd85c74a52084d321d856aa3
Miyanber <miyanba2984h@gmail.com>
2026-03-14 22:14:02 +0900

    出力構成変更

--- build_release.py
+++ build_release.py
@@ -11,7 +11,6 @@ release_dir = "release"
 exe_path = os.path.join(dist_dir, "main.exe")
 cred_path = "credentials.json"
 
-# 1. PyInstaller 実行
 subprocess.run([
     "pyinstaller",
     "main.py",
@@ -19,19 +18,15 @@ subprocess.run([
     "--add-data", "materials;materials"
 ], check=True)
 
-# 2. release フォルダ作り直し
 if os.path.exists(release_dir):
     shutil.rmtree(release_dir)
 
 os.makedirs(release_dir)
 
-# 3. exeコピー
+shutil.copy(os.path.join(dist_dir, "README.txt")),
 shutil.copy(exe_path, os.path.join(release_dir, "main.exe"))
-
-# 4. credentialsコピー
 shutil.copy(cred_path, os.path.join(release_dir, "credentials.json"))
 
-# 5. zip作成
 zip_name = f"{APP_NAME}.zip"
 
 with zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED) as z:
